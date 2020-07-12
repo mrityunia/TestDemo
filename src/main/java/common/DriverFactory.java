561ca298a65df28074bc2e6ba90599214c237134
Mrityunjoy Chowdhury <mchowdhury891@outlook.com>
2020-07-12 19:24:11 +0530

    adding remort driver

--- src/main/java/common/DriverFactory.java
+++ src/main/java/common/DriverFactory.java
@@ -1,21 +1,40 @@
 package common;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class DriverFactory {
 	
 	
 public WebDriver getDriver(String browsername) {
-	WebDriver driver;
+	//WebDriver driver;
+	RemoteWebDriver driver=null;
 	
 	if("chrome".equalsIgnoreCase(browsername)) {
 		
-		System.setProperty("webdriver.chrome.driver","chromedriver");
-		ChromeOptions chromeOptions = new ChromeOptions();
+		//System.setProperty("webdriver.chrome.driver","chromedriver");
+		//ChromeOptions chromeOptions = new ChromeOptions();
 		//chromeOptions.addArguments("--headless");
-		driver=new ChromeDriver();
+		
+		URL url;
+		try {
+			url = new URL("http://localhost:4444/wd/hub");
+			DesiredCapabilities capbility = DesiredCapabilities.chrome();
+			driver= new RemoteWebDriver(url,capbility);
+			
+			driver=new ChromeDriver();
+			
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 	else {
 		
